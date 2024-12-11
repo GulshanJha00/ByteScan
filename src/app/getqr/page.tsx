1ea1ab011bfacb706f12229a7e1f23ccef24b40c
GulshanJha00 <gk4763549@gmail.com>
2024-12-12 01:24:25 +0530

    basic added

--- src/app/getqr/page.tsx
+++ src/app/getqr/page.tsx
@@ -1,61 +1,64 @@
-'use client'
-import React, { useState } from 'react'
-import { SignedIn, SignedOut, SignIn } from '@clerk/nextjs'
-import QRCode from 'qrcode'
-
-
+"use client";
+import React, { useState } from "react";
+import { SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
+import QRCode from "qrcode";
+import Image from "next/image";
 const Page = () => {
-  const [input, setInput] = useState('')
-  
+  const [input, setInput] = useState("");
+  const [qrCode, setQrCode] = useState('')
+
   const generateQRCode = async () => {
     if (!input) {
-      alert('Please enter valid input!');
+      alert("Please enter valid input!");
       return;
     }
     try {
       const qr = await QRCode.toDataURL(input);
-      
+
+      setQrCode(qr)
       console.log(qr);
     } catch (err) {
       console.error(err);
     }
-  }
-
-
+  };
 
   return (
-    <>
-    <div className='flex bg-gray-800 justify-center items-center h-screen absolute left-1/3'>
+    <div className="bg-[url]">
+      <div className="flex justify-center items-center h-screen absolute left-1/3">
+        <SignedOut>
+          <SignIn routing="hash" />
+        </SignedOut>
+      </div>
+      <SignedIn>
+        <div className="bg-gray-800 h-screen flex justify-center items-center gap-7">
+          <div >
 
-    <SignedOut>
-      <SignIn routing='hash' />
-    </SignedOut>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Enter text or URL"
+              className="w-1/2 p-3 border bg-yellow-50 border-gray-300 rounded-md text-blue-600"
+            />
+
+            <button
+              onClick={generateQRCode}
+              className="bg-blue-500 text-white px-4 py-2 rounded mt-3"
+            >
+              Generate
+            </button>
+          </div>
+          
+          {qrCode?<div>
+            <Image src={qrCode} height={100} width={100} alt="image"></Image>
+          </div>
+          :
+          <div className="hidden"></div>}
+      </div>
+
+      </SignedIn>
     </div>
-    <SignedIn>
+  );
+};
 
-
-    <div>
-
-      <input
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Enter text or URL"
-            className="w-1/2 p-3 border border-gray-300 rounded-md text-blue-600"
-          />
-
-<button
-          onClick={generateQRCode}
-          className="bg-blue-500 text-white px-4 py-2 rounded mt-3"
-        >
-          Generate QR Code
-        </button>
-    </div>
-
-
-    </SignedIn>
-    </>
-  )
-}
-
-export default Page
+export default Page;
